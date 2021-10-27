a678829bb9a94f4b033f776577a4acccca324e41
Wallace Junior <wallaceoliveirarn@gmail.com>
2021-10-27 20:03:49 -0300

    criando a tabela de multiplicacaogit status

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,17 @@
-function nomeDaFuncao() { 
-    // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
-    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-    let x = [];
-    for (let i = 0; i <= 10; i++) {
-        x[i] = [];
-    }
-    console.table(x)
+function tabelaMultiplicacao(num){
+
+        let meuArray = [];
+      
+      for(let contador = 0; contador < num+1; contador++){
+        meuArray.push([]);
+        
+        for(let subContador = 0; subContador < num+1; subContador++){
+        meuArray[contador].push(contador * subContador);
+      
+      }
+      }
+      
+      return meuArray
+      }
+      console.table(tabelaMultiplicacao(12));
 }
